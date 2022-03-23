bcbe8c0cb7067bd61acaabfcebf4b9f928e661a5
jojoma8 <jojo_ma@hotmail.co.uk>
2022-03-23 23:20:13 +0000

    econ

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import tg
+import econ
+# import requests
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    return "API is working fine 444"
+
+
+# @app.route("/<query>")
+# def telegram_(query):
+#     return jsonify(tg.telegram(query))
+
+@app.route("/<query>")
+def read_(query):
+    return jsonify(tg.read(query))
+
+
+if __name__ == "__main__":
+    # app.debug = True
+    app.debug = False
+    # app.run(host="0.0.0.0", port=5000)
---
+++ econ.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup as bs
+import requests
+# import pandas as pd
+import re
+
+
+def read(url):
+    try:
+        r = requests.get(
+            'https://www.economist.com/leaders/2021/03/31/how-europe-has-mishandled-the-pandemic')
+        s = bs(r.text, "html.parser")
+        p = [x.get_text() for x in s.find_all(class_="article__body-text")]
+        data = {}
+        data['content'] = p
+        print(p)
+        return data
+    except Exception as e:
+        return {"status": False, "error": e}
+
+
+read('text')
