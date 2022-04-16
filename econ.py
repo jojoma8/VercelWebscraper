74898cdcd693fc7de288339548dfde1924164260
jojoma8 <jojo_ma@hotmail.co.uk>
2022-04-16 22:01:59 +0100

    test4

--- econ.py
+++ econ.py
@@ -15,7 +15,8 @@ def read(url):
             base_url)
         s = bs(r.text, "html.parser")
         p = [x.get_text() for x in s.find_all(class_="article__body-text")]
-        article_headline = [s.find_all(class_="article__headline")]
+        article_headline = [x.get_text()
+                            for x in s.find_all(class_="article__headline")]
         data = {}
         data['content'] = p
         data['article_headline'] = article_headline
